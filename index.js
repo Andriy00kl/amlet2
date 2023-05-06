5d07f68f6f53a08d4530989da15c5cdedb3d8b77
unknown <shevchenko.and2010@gmail.com>
2023-05-06 17:45:37 +0300

    input

--- index.js
+++ index.js
@@ -10,13 +10,37 @@ const connection = mysql.createConnection({
 })
 
 const server = http.createServer((req, res) => {
+    if(req.url === '/message'){
     connection.query(
-        'SELECT * FROM user',
+        'SELECT * FROM message',
         function(err, result, fields){
-            res.write(`JSON.stringify(result)`);
-            res.end();
-        }
-    );
-})
+                let html = `<html><body><ul>`
+                result.forEach((element) =>{
+                    html += `<li>${element.content}</li>`
+                });
+                html += `</ul><form action="/add" method="POST"> <input type="text" name="content"></form></body></html>`
+                res.writeHead(200, {'Content-type': 'text/html'});
+                res.end(html);
+            }
+        );
+    }else if(req.url === "/add" && req.method === 'POST'){
+        let data = '';
+        req.on('data', function(chunk){
+            data += chunk;
+        });
+        req.on('data', function(chunk){
+            let sp = new URLSearchParams(data);
+            let content = sp.get('content');
+            connection.query(
+                `INSERT INTO message(content, author_id, dialog_id) VALUES("${content}", 1, 5);`,
+                function (err, result, fields){
+                    res.writeHead(302, {'Location': '/message'});
+                    res.end();
+                }
+            );   
+        });
+    }
+});
+    
 
 server.listen(666);
